78d304124fe6f7eefb047269d7e807a9b9e66586
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-06-30 15:37:41 -0400

    Write getComputerChoice method
    Write getComputerChoice method to randomly generate
    a choice of rock, paper, scissors.

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+  let result = Math.floor(Math.random() * 3);
+  if (result == 0) {
+    return "Rock";
+  }
+  else if (result == 1) {
+    return "Paper";
+  }
+  else {
+    return "Scissors";
+  }
+}
