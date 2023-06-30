aa28957c702f80ccdeff46df88d90423ebdc6a31
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-06-30 15:50:23 -0400

    Make playRound function to determine winner
    Make playRound function that accepts user input and computer
    input and returns result of the game.

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+  let result;
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  }
+  else if (playerSelection === 'Rock') {
+    if (computerSelection === 'Paper') {
+      result = "Lose";
+    }
+    else {
+      result = 'Win';
+    }
+  }
+  else if (playerSelection === 'Paper') {
+    if (computerSelection === 'Rock') {
+      result = 'Win';
+    }
+    else {
+      result = 'Lose';
+    }
+  }
+  else {
+    if (computerSelection === 'Rock') {
+      result = 'Lose';
+    }
+    else {
+      result = 'Win';
+    }
+  }
+
+  if (result === 'Win') {
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+  }
+  else {
+    return "You Lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
